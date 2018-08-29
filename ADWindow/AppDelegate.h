92ca11f1f809a9b524f9095005a582aed6a99b4a
liangruibin <liangruibin@cedarhd.com>
2018-08-29 14:16:49 +0800

    Initial Commit

---
+++ ADWindow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ADWindow
+//
+//  Created by lrb on 2018/8/29.
+//  Copyright © 2018年 lrb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ADWindow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ADWindow
+//
+//  Created by lrb on 2018/8/29.
+//  Copyright © 2018年 lrb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
